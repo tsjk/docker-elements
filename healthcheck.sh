705aff20c84af6cca0c1d6cd43c6c6f23794a43e
Tamas Jantvik <tsjk@hotmail.com>
2023-12-14 16:40:52 +0100

    updated for congruency

--- healthcheck.sh
+++ healthcheck.sh
@@ -7,9 +7,11 @@
 CHAIN="$1"; shift 1
 [ -s "/var/lib/elementsd/$CHAIN/.cookie" ] || exit 1
 
+cli() { timeout -k 60s 50s elements-cli -conf="/etc/elements/elements.conf" -datadir="/var/lib/elementsd" -chain="$CHAIN" -rpccookiefile="/var/lib/elementsd/$CHAIN/.cookie" "$@"; }
+
 get_getblockchaininfo_value_for_key() {
   [ -n "$1" ] && \
-    timeout -k 60s 50s elements-cli -conf="/etc/elements/elements.conf" -datadir="/var/lib/elementsd/$CHAIN" -rpccookiefile="/var/lib/elementsd/$CHAIN/.cookie" getblockchaininfo | \
+    cli getblockchaininfo | \
     sed -e 's/[{}]//g' -e 's/^\s\s*//g' -e '/^\s*$/d' | \
     awk '{ n = split($0, a, ","); for (i=1; i<=n; i++) print a[i]; }' | \
     sed  -e '/^\s*$/d' -e 's/\(^"\S\S*":\)\s\s*/\1/' | \
@@ -19,14 +21,16 @@ get_getblockchaininfo_value_for_key() {
 [ "$(get_getblockchaininfo_value_for_key 'initialblockdownload')" != "true" ] || exit 1
 [ "$(get_getblockchaininfo_value_for_key 'headers')" = "$(get_getblockchaininfo_value_for_key 'blocks')" ] || exit 1
 
-block_count=$(timeout -k 60s 50s elements-cli -conf="/etc/elements/elements.conf" -datadir="/var/lib/elementsd/$CHAIN" -rpccookiefile="/var/lib/elementsd/$CHAIN/.cookie" getblockcount 2> /dev/null)
+block_count=$(cli getblockcount 2> /dev/null)
 [ -n "$block_count" ] || exit 1
-latest_block_hash=$(timeout -k 60s 50s elements-cli -conf="/etc/elements/elements.conf" -datadir="/var/lib/elementsd/$CHAIN" -rpccookiefile="/var/lib/elementsd/$CHAIN/.cookie" getblockhash $block_count)
-[ -n "$latest_block_hash" ] || exit 1
-latest_block_time_stamp=$(timeout -k 60s 50s elements-cli -conf="/etc/elements/elements.conf" -datadir="/var/lib/elementsd/$CHAIN" -rpccookiefile="/var/lib/elementsd/$CHAIN/.cookie" getblock $latest_block_hash | awk -F ':' '($1 ~ /\s*"time"/) { sub(/^\s*/, "", $2); sub(/,$/, "", $2); print $2; }')
-[ -n "$latest_block_time_stamp" ] || exit 1
-latest_block_age=$(( $(date '+%s') - $latest_block_time_stamp ))
 
+latest_block_hash=$(cli getblockhash $block_count 2> /dev/null)
+[ -n "$latest_block_hash" ] || exit 1
+
+latest_block_time_stamp=$(cli getblock $latest_block_hash 2> /dev/null | awk -F ':' '($1 ~ /\s*"time"/) { sub(/^\s*/, "", $2); sub(/,$/, "", $2); print $2; }')
+[ -n "$latest_block_time_stamp" ] || exit 1
+
+latest_block_age=$(( $(date '+%s') - $latest_block_time_stamp ))
 [ $latest_block_age -lt 300 ] || exit 1
 
 exit 0
